242d40e85eecd343636ac5285467be6157f71086
LaylaJeragh <110116284+LaylaJeragh@users.noreply.github.com>
2023-09-06 19:41:01 +0300

    layla

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 # write your code here
+person = {
+    "name" : "Layla",
+    "age" : 15,
+    "hobbies" : ["Digital art", "Crocheting", "Sewing", "Baking", "Coding", "Painting", "Writing"]
+}
+print (person["name"])
+print (person["age"])
+person["age"] = 13
+person["country"] = "Kuwait"
+print(person)
+print(len(person))
+person["hobbies"].append("Swimming")
+def check_hobbies(person):
+    if len(person["hobbies"])>3:
+      print("WOW YOU ARE AMAZING")
+    else:
+       print("Nice")
+  
+check_hobbies()
